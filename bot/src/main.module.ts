710eebf5c940182a63287ccd31a59a83786e8281
Evan Lynch <e.lynch303@gmail.com>
2020-03-23 22:46:16 -0400

    add timed check to look for new installs

--- bot/src/main.module.ts
+++ bot/src/main.module.ts
@@ -5,106 +5,115 @@ import fetch from 'node-fetch';
 import * as config from './config.json';
 
 const pubnub = new PubNub({ subscribeKey: config.subscribeKey });
+const listener = {
+    message: function (m) {
+        // handle messages
+        //console.log(m);
+        let channelName = m.channel; // The channel for which the message belongs
+        let channelGroup = m.subscription; // The channel group or wildcard subscription match (if exists)
+        let pubTT = m.timetoken; // Publish timetoken
+        let msg = m.message; // The Payload
+        let publisher = m.publisher; //The Publisher
+        channelName = channelName.replace('chat.', '');
+        messageHandler(msg, channelName);
+    },
+    presence: function (p) {
+        // handle presence
+        var action = p.action; // Can be join, leave, state-change or timeout
+        var channelName = p.channel; // The channel for which the message belongs
+        var occupancy = p.occupancy; // No. of users connected with the channel
+        var state = p.state; // User State
+        var channelGroup = p.subscription; //  The channel group or wildcard subscription match (if exists)
+        var publishTime = p.timestamp; // Publish timetoken
+        var timetoken = p.timetoken; // Current timetoken
+        var uuid = p.uuid; // UUIDs of users who are connected with the channel
+        if (action != "interval") {
+            console.log("presence",p);
+        }
+    },
+    signal: function (s) {
+        // handle signal
+        console.log("signal",s);
+        var channelName = s.channel; // The channel for which the signal belongs
+        var channelGroup = s.subscription; // The channel group or wildcard subscription match (if exists)
+        var pubTT = s.timetoken; // Publish timetoken
+        var msg = s.message; // The Payload
+        var publisher = s.publisher; //The Publisher
+    },
+    user: function (userEvent) {
+        console.log("userEvent",userEvent);
+        // for Objects, this will trigger when:
+        // . user updated
+        // . user deleted
+    },
+    space: function (spaceEvent) {
+        console.log("space",spaceEvent);
+        // for Objects, this will trigger when:
+        // . space updated
+        // . space deleted
+    },
+    membership: function (membershipEvent) {
+        console.log("membership",membershipEvent);
+        // for Objects, this will trigger when:
+        // . user added to a space
+        // . user removed from a space
+        // . membership updated on a space
+    },
+    messageAction: function (ma) {
+        console.log("messageAction",ma);
+        // handle message action
+        var channelName = ma.channel; // The channel for which the message belongs
+        var publisher = ma.publisher; //The Publisher
+        var event = ma.message.event; // message action added or removed
+        var type = ma.message.data.type; // message action type
+        var value = ma.message.data.value; // message action value
+        var messageTimetoken = ma.message.data.messageTimetoken; // The timetoken of the original message
+        var actionTimetoken = ma.message.data.actionTimetoken; //The timetoken of the message action
+    },
+    status: function (s) {
+        //console.log("status",s);
+        var affectedChannelGroups = s.affectedChannelGroups; // The channel groups affected in the operation, of type array.
+        var affectedChannels = s.affectedChannels; // The channels affected in the operation, of type array.
+        var category = s.category; //Returns PNConnectedCategory
+        var operation = s.operation; //Returns PNSubscribeOperation
+        var lastTimetoken = s.lastTimetoken; //The last timetoken used in the subscribe request, of type long.
+        var currentTimetoken = s.currentTimetoken; //The current timetoken fetched in the subscribe response, which is going to be used in the next request, of type long.
+        var subscribedChannels = s.subscribedChannels; //All the current subscribed channels, of type array.
+    }
+};
+
 let initurl = 'https://api.theta.tv/v1/oauth_application/' + config.clientId + '/oauth_install/list?client_id=' + config.clientId + '&client_secret=' + config.clientSecret;
-fetch(initurl).then(res => res.text()).then(body => init(body));
+
+function getInstalls(){
+    fetch(initurl).then(res => res.text()).then(body => init(body));
+}
 
 function init(body) {
     let data = JSON.parse(body);
-    data.body.forEach(function (channel) {
-        config.channels.subscribers.push("chat." + channel.user_id);
+    config.subscribers.splice(0,config.subscribers.length);
+    data.body.forEach(function (channel){
         config.channels[channel.user_id] = {
             clientId: channel.client_id,
             userId: channel.user_id,
             accessToken: channel.access_token,
         };
     });
+    for(let channel in config.channels){
+        config.subscribers.push("chat." + channel);
+    }
+    //console.log(config);//ONLY for debuging
     startPubNub();
 }
 
 function startPubNub() {
     pubnub.subscribe({
-        channels: config.channels.subscribers,
+        channels: config.subscribers,
         withPresence: true
     });
-    pubnub.addListener({
-        message: function (m) {
-            // handle messages
-            //console.log(m);
-            let channelName = m.channel; // The channel for which the message belongs
-            let channelGroup = m.subscription; // The channel group or wildcard subscription match (if exists)
-            let pubTT = m.timetoken; // Publish timetoken
-            let msg = m.message; // The Payload
-            let publisher = m.publisher; //The Publisher
-            channelName = channelName.replace('chat.', '');
-            messageHander(msg, channelName);
-        },
-        presence: function (p) {
-            // handle presence
-            var action = p.action; // Can be join, leave, state-change or timeout
-            var channelName = p.channel; // The channel for which the message belongs
-            var occupancy = p.occupancy; // No. of users connected with the channel
-            var state = p.state; // User State
-            var channelGroup = p.subscription; //  The channel group or wildcard subscription match (if exists)
-            var publishTime = p.timestamp; // Publish timetoken
-            var timetoken = p.timetoken; // Current timetoken
-            var uuid = p.uuid; // UUIDs of users who are connected with the channel
-            if (action != "interval") {
-                console.log(p);
-            }
-        },
-        signal: function (s) {
-            // handle signal
-            console.log(s);
-            var channelName = s.channel; // The channel for which the signal belongs
-            var channelGroup = s.subscription; // The channel group or wildcard subscription match (if exists)
-            var pubTT = s.timetoken; // Publish timetoken
-            var msg = s.message; // The Payload
-            var publisher = s.publisher; //The Publisher
-        },
-        user: function (userEvent) {
-            console.log(userEvent);
-            // for Objects, this will trigger when:
-            // . user updated
-            // . user deleted
-        },
-        space: function (spaceEvent) {
-            console.log(spaceEvent);
-            // for Objects, this will trigger when:
-            // . space updated
-            // . space deleted
-        },
-        membership: function (membershipEvent) {
-            console.log(membershipEvent);
-            // for Objects, this will trigger when:
-            // . user added to a space
-            // . user removed from a space
-            // . membership updated on a space
-        },
-        messageAction: function (ma) {
-            console.log(ma);
-            // handle message action
-            var channelName = ma.channel; // The channel for which the message belongs
-            var publisher = ma.publisher; //The Publisher
-            var event = ma.message.event; // message action added or removed
-            var type = ma.message.data.type; // message action type
-            var value = ma.message.data.value; // message action value
-            var messageTimetoken = ma.message.data.messageTimetoken; // The timetoken of the original message
-            var actionTimetoken = ma.message.data.actionTimetoken; //The timetoken of the message action
-        },
-        status: function (s) {
-            console.log(s);
-            var affectedChannelGroups = s.affectedChannelGroups; // The channel groups affected in the operation, of type array.
-            var affectedChannels = s.affectedChannels; // The channels affected in the operation, of type array.
-            var category = s.category; //Returns PNConnectedCategory
-            var operation = s.operation; //Returns PNSubscribeOperation
-            var lastTimetoken = s.lastTimetoken; //The last timetoken used in the subscribe request, of type long.
-            var currentTimetoken = s.currentTimetoken; //The current timetoken fetched in the subscribe response, which is going to be used in the next request, of type long.
-            var subscribedChannels = s.subscribedChannels; //All the current subscribed channels, of type array.
-        }
-    });
+    pubnub.addListener(listener);
 }
 
-function messageHander(msg, channel) {
+function messageHandler(msg, channel) {
     let msgText = msg.data.text;
     let msgType = msg.type;
     let user = msg.data.user;
@@ -236,3 +245,5 @@ function getUserNameFromId(userId) {
     return username;
 
 }
+
+setInterval(getInstalls,5000);
